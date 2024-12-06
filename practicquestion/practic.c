cd5feaa324eb16d613a648ef1fc2e612c03ca858
Your Name <soumenbhandari25@gmail.com>
2024-12-06 15:42:35 +0000

    some practic on practic.c

--- practicquestion/practic.c
+++ practicquestion/practic.c
@@ -1,19 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    FILE *fp = fopen("text.txt", "w"); // Open for reading
-    if (fp == NULL) {
-        printf("File not found or unable to open.\n");
-        return 1;
-    }
- char str[100] = "Soumen Bhandari\n";
-    fputs(str, fp);
-    fclose(fp); // Close the file after writing
-    char ch;
-    printf("Contents of the file:\n");
-    while ((ch = fgetc(fp)) != EOF) {
-        putchar(ch); // Print each character to the console
-    }
-    fclose(fp); // Close the file
+    char str[100];
+    printf("Enter a string: ");
+    gets(str);  // Reads a string from the user (with spaces)
+    printf("You entered: %s\n", str);
     return 0;
 }
